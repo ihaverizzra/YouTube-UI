904562ff5fec09535af0cc49fc8606f8c6032b38
ihaverizzra <163343096+ihaverizzra@users.noreply.github.com>
2025-11-14 20:50:18 -0500

    Refactor App component and remove lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './utils/store';
-import "../src/index.css";
-import Header from './Components/Header'
-import Body from './HomePageContainer/Body'
-import SearchResults from './HomePageContainer/SearchResults';
-import MainContainer from './Components/MainContainer'
-import SubScriptionPage from './HomePageContainer/SubScriptionPage';
-import ExploreVideoPage from './HomePageContainer/ExploreVideoPage';
-
-// Lazy load WatchPage
-const WatchPage = lazy(() => import('./HomePageContainer/WatchPage'));
+import "./index.css";
+import Header from './Components/Header';
+import Body from './HomePageContainer/Body';
+import MainContainer from './Components/MainContainer';
+import WatchPage from './HomePageContainer/WatchPage';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <Router>
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Body />}>
-              <Route index element={<MainContainer />} />
-              <Route exact path="/watch"
-                element={
-                  <Suspense fallback={<h1>Loading...</h1>}>
-                    <WatchPage />
-                  </Suspense>
-                }
-              />
-              <Route exact path="/results" element={<SearchResults />} />
-              <Route exact path='/explore' element={<ExploreVideoPage />} />
-              <Route exact path='/channel' element={<SubScriptionPage />} />
-            </Route>
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route index element={<MainContainer />} />
+            <Route path="/watch" element={<WatchPage />} />
+          </Route>
+        </Routes>
+      </Router>
     </Provider>
   );
 }
